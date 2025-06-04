305558094b561f1b13e192355457f2e40bbd8cb4
Burak Dansık <bburakdnsk@gmail.com>
2025-06-04 04:47:39 +0300

    Home Screen

---
+++ CourseApp/src/Screens/HomeScreen.js
@@ -0,0 +1,10 @@
+import { StyleSheet, Text, View } from "react-native";  
+import React from 'react';
+
+export default function HomeScreen(){
+    return(
+        <View>
+            <Text>Home Screens </Text>
+        </View>
+    )
+}
